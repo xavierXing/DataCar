560dd573744fcf6aa04580a4f09d3f722571b45b
邢浩 <xxxinghao@sina.com>
2017-06-10 16:53:58 +0800

    Initial Commit

---
+++ DataCar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DataCar
+//
+//  Created by 邢浩 on 2017/6/10.
+//  Copyright © 2017年 邢浩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
